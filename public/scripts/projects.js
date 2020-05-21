06e9a54a5ed47d0bed3d924e299c319eb1b3a335
Cole Connelly <cdconn00@tamu.edu>
2020-05-21 11:53:16 -0500

    Create projects page.

---
+++ public/scripts/projects.js
@@ -0,0 +1,23 @@
+// JS File for Projects View
+
+
+$(function () {
+	// navbar configuration
+	$(document).scroll(function () {
+		$("#navbar").toggleClass('scrolled', $(this).scrollTop() > $("#navbar").height());
+	});
+
+	$('.navbar-toggler').on('click', function() {
+		$("#navbar").addClass('scrolled');
+	});
+
+	// animate news card section
+	$(window).on('load', function() {
+		$('.project-module').hover(function() {
+		    $(this).find('.description').stop().animate({
+		        height: "toggle",
+		        opacity: "toggle"
+		    }, 300);
+		});
+	});
+});
