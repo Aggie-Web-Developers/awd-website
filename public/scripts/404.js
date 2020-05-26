b9752869f1939eb7717ef4374d45f59ecc2c9541
christopher_pc <christopherjmacgregor@gmail.com>
2020-05-26 10:27:53 -0500

    404 page

---
+++ public/scripts/404.js
@@ -0,0 +1,13 @@
+$(function () {
+  document.title = 'AWD - 404';
+	$('#divNav').addClass('notFound-banner');
+
+	// navbar configuration
+	$(document).scroll(function () {
+		$("#navbar").toggleClass('scrolled', $(this).scrollTop() > $("#navbar").height());
+	});
+
+	$('.navbar-toggler').on('click', function() {
+		$("#navbar").addClass('scrolled');
+	});
+});
