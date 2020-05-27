ed1d71964361bd0e2c16a2f68560c72a456d87d6
Cole Connelly <cdconn00@tamu.edu>
2020-05-27 16:59:18 -0500

    Add email template for styling of contact us form notifications.

--- email/email.js
+++ email/email.js
@@ -6,6 +6,13 @@ aws.config.loadFromPath(path.join(__dirname, '../config.json'));
 var ses = new aws.SES();
 
 mailerObj.sendContactUsEmailGen = function(formData){
+  var content = "<p><b>Name:</b> " + formData.txtNameGen + "</p>";
+  content    += "<p><b>Email:</b> " + formData.txtEmailGen + "</p>";
+  content    += "<p><b>Subject:</b> " + formData.ddlSubjectGen + "</p>";
+  content    += "<p><b>Comments:</b></p><p>" + formData.txtCommentsGen + "</p>";
+
+  content = content.replace(/(?:\r\n|\r|\n)/g, '<br>');
+
   var params = {
     Destination: {
       BccAddresses: [
@@ -13,29 +20,15 @@ mailerObj.sendContactUsEmailGen = function(formData){
       ],
       ToAddresses: []
     },
-    Message: {
-      Body: {
-        Html: {
-         Charset: "UTF-8",
-         Data: 'New General Contact Us Form Submitted.'
-        },
-        Text: {
-         Charset: "UTF-8",
-         Data: 'New General Contact Us Form Submitted.'
-        }
-       },
-       Subject: {
-        Charset: 'UTF-8',
-        Data: 'New General Contact Us Form'
-       }
-      },
     Source: 'Aggie Web Developers <no-reply@aggiedevelopers.com>',
+    Template: "AWD-Contact-Us-Notification-Template",
+    TemplateData: "{ \"title\": \"New General Contact Us Submission\", \"content\": \"" + content.replace("\n", "< /br>") + "\" }",
     ReplyToAddresses: [
        'no-reply@aggiedevelopers.com',
     ],
   };
 
-  var sendPromise = new aws.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
+  var sendPromise = new aws.SES({apiVersion: '2010-12-01'}).sendTemplatedEmail(params).promise();
 
   sendPromise.then(
     function(data) {}).catch(
@@ -45,6 +38,14 @@ mailerObj.sendContactUsEmailGen = function(formData){
 }
 
 mailerObj.sendContactUsEmailCorp = function(formData){
+  var content = "<p><b>Name:</b> " + formData.txtNameCorp + "</p>";
+  content    += "<p><b>Email:</b> " + formData.txtEmailCorp + "</p>";
+  content    += "<p><b>Company:</b> " + formData.txtCorp + "</p>";
+  content    += "<p><b>Subject:</b> " + formData.ddlSubjectCorp + "</p>";
+  content    += "<p><b>Comments:</b></p><p>" + formData.txtCommentsCorp + "</p>";
+
+  content = content.replace(/(?:\r\n|\r|\n)/g, '<br>');
+
   var params = {
     Destination: {
       BccAddresses: [
@@ -52,29 +53,15 @@ mailerObj.sendContactUsEmailCorp = function(formData){
       ],
       ToAddresses: []
     },
-    Message: {
-      Body: {
-        Html: {
-         Charset: "UTF-8",
-         Data: 'New Corporate Contact Us Form Submitted'
-        },
-        Text: {
-         Charset: "UTF-8",
-         Data: 'New Corporate Contact Us Form Submitted'
-        }
-       },
-       Subject: {
-        Charset: 'UTF-8',
-        Data: 'New Corporate Contact Us Form Submitted'
-       }
-      },
     Source: 'Aggie Web Developers <no-reply@aggiedevelopers.com>',
+    Template: "AWD-Contact-Us-Notification-Template",
+    TemplateData: "{ \"title\": \"New Corporate Contact Us Submission\", \"content\": \"" + content.replace("\n", "< /br>") + "\" }",
     ReplyToAddresses: [
        'no-reply@aggiedevelopers.com',
     ],
   };
 
-  var sendPromise = new aws.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
+  var sendPromise = new aws.SES({apiVersion: '2010-12-01'}).sendTemplatedEmail(params).promise();
 
   sendPromise.then(
     function(data) {}).catch(
