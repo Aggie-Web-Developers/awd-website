22e7b8434d29739a8982ee1f621b14f4cd110ec1
Cole Connelly <cdconn00@tamu.edu>
2020-07-07 09:37:01 -0500

    Add improved error filing to portal email CRUD routes.

--- routes/portal/emails.js
+++ routes/portal/emails.js
@@ -6,42 +6,44 @@ var express    = require("express"),
     email      = require('../../email/email');
 
 router.get('/', middleware.checkAuthenticated, function(req, res) {
-	var queryText =" SELECT em.*, e.name as event_name FROM tbl_emails em LEFT JOIN tbl_events e ON e.id = em.event_id ORDER BY em.deleted ASC, em.create_date DESC";
+	var sqlQuery = "SELECT em.*, e.name as event_name FROM tbl_emails em LEFT JOIN tbl_events e ON e.id = em.event_id ORDER BY em.deleted ASC, em.create_date DESC";
 
-	var sqlReq = new sql.Request().query(queryText, (err, result) => {
-		if (err){
-			console.log(err)
-			req.flash("error", "Error loading emails.");
-		} else {
-			res.render('portal/emails/index', { emails: result.recordset });
-		}
+	var sqlReq = new sql.Request().query(sqlQuery).then(result => {
+		res.render('portal/emails/index', { emails: result.recordset });
+	}).catch(err => {
+		req.flash("error", "Error loading emails.");
+		res.render('portal/emails/index', { emails: [] });
 	});
 });
 
 router.get('/edit/:id', middleware.checkAuthenticated, function(req, res) {
-	var sqlReq = new sql.Request().input("id", sql.Int, req.params.id).query("SELECT TOP 1 * FROM tbl_emails WHERE id = @id", (err, result) => {
-		if (err){
-			req.flash("error", "Error loading emails.");
+	var sqlReq = new sql.Request().input("id", sql.Int, req.params.id).query("SELECT TOP 1 * FROM tbl_emails WHERE id = @id")
+		.then(result => {
+			if (result.recordset.length > 0) {
+				res.render('portal/emails/edit', { email: result.recordset[0] });
+			} else {
+				req.flash("error", "Error email does not exist.");
+				res.redirect("/portal/emails/");
+			}
+		}).catch(err => {
+			req.flash("error", "Error loading selected email.");
 			res.redirect("/portal/emails/");
-		} else {
-			res.render('portal/emails/edit', { email: result.recordset[0] });
-		}
 	});
 });
 
 router.put('/edit/:id', middleware.checkAuthenticated, function(req, res) {
 	var sqlReq = new sql.Request();
-	var queryText = "";
+	var sqlQuery = "";
 	
 	if (req.body.txtSendDate != "") {
 		var user_date = new Date(req.body.txtSendDate + " " + req.body.txtSendTime);
 		var converted_date = new Date(user_date.getTime()).toISOString().slice(0, 19).replace('T', ' ');
 		sqlReq.input("send_date", sql.NVarChar, converted_date);
 
-		queryText = "UPDATE tbl_emails " +
+		sqlQuery = "UPDATE tbl_emails " +
 			        "SET subject = @subject, deleted = @deleted, recip_type = @recip_type, body = @body, sending_user_id = @sending_user_id, send_date = @send_date WHERE id = @id";
 	} else {
-		queryText = "UPDATE tbl_emails " +
+		sqlQuery = "UPDATE tbl_emails " +
 			        "SET subject = @subject, deleted = @deleted, recip_type = @recip_type, body = @body, sending_user_id = @sending_user_id WHERE id = @id";
 	}
 
@@ -52,14 +54,17 @@ router.put('/edit/:id', middleware.checkAuthenticated, function(req, res) {
 	sqlReq.input("body", sql.NVarChar, req.body.txtBody);
 	sqlReq.input("sending_user_id", sql.Int, req.user.id);
 
-	sqlReq.query(queryText, (err, result) => {
-		if (err || result.rowsAffected == 0){
-			console.log(err)
+	sqlReq.query(sqlQuery)
+		.then(result => {
+			if (result.rowsAffected == 0){
+				req.flash("error", "Error updating email.");
+			} else {
+				req.flash("success", "Success! Email updated.");
+				res.redirect("/portal/emails/");
+			}
+		}).catch(err => {
 			req.flash("error", "Error updating email.");
-		} else {
-			req.flash("success", "Success! Email updated.");
 			res.redirect("/portal/emails/");
-		}
 	});
 });
 
@@ -69,20 +74,19 @@ router.get('/new', middleware.checkAuthenticated, function(req, res) {
 
 router.post('/new', middleware.checkAuthenticated, function(req, res) {
 	var sqlReq = new sql.Request();
-	var queryText = "";
+	var sqlQuery = "";
 	
-
 	// If user has specified a date to send the email at
 	if (req.body.txtSendDate != "") {
 		var user_date = new Date(req.body.txtSendDate + " " + req.body.txtSendTime);
 		var converted_date = new Date(user_date.getTime()).toISOString().slice(0, 19).replace('T', ' ');
 		sqlReq.input("send_date", sql.NVarChar, converted_date);
 
-		queryText = "INSERT INTO tbl_emails " +
+		sqlQuery = "INSERT INTO tbl_emails " +
 			        "(subject, deleted, recip_type, body, sending_user_id, send_date) values " +
 		     	    "(@subject, @deleted, @recip_type, @body, @sending_user_id, @send_date) ";
 	} else {
-		queryText = "INSERT INTO tbl_emails " +
+		sqlQuery = "INSERT INTO tbl_emails " +
 			        "(subject, deleted, recip_type, body, sending_user_id) values " +
 		     	    "(@subject, @deleted, @recip_type, @body, @sending_user_id) ";
 	}
@@ -93,14 +97,17 @@ router.post('/new', middleware.checkAuthenticated, function(req, res) {
 	sqlReq.input("body", sql.NVarChar, req.body.txtBody);
 	sqlReq.input("sending_user_id", sql.Int, req.user.id);
 
-	sqlReq.query(queryText, (err, result) => {
-		if (err || result.rowsAffected == 0){
-			console.log(err)
+	sqlReq.query(sqlQuery)
+		.then(result => {
+			if (result.rowsAffected == 0){
+				req.flash("error", "Error creating email.");
+			} else {
+				req.flash("success", "Success! Email created.");
+				res.redirect("/portal/emails/");
+			}
+		}).catch(err => {
 			req.flash("error", "Error creating email.");
-		} else {
-			req.flash("success", "Success! Email created.");
 			res.redirect("/portal/emails/");
-		}
 	});
 });
 
