1f3d478942689a28415693499ab94f32dcdbba9e
Cole D. Connelly <cdconn00@gmail.com>
2020-08-22 11:00:19 -0500

    Add Open Sans to summernote for emails, fix console errors with summer note.

--- public/scripts/portal/emails/new.js
+++ public/scripts/portal/emails/new.js
@@ -1,13 +1,12 @@
 // JS File for Portal Email New View
 
 $(function () {
-	$('#txtBody').summernote();
+	setupSummerNote();
 
 	$('#frm').validate({
-		ignore: ':hidden',
+		ignore: ':hidden, .note-editor *',
 		rules: {
 			txtSubject: { required: true, maxlength: 50 },
-			txtBody: { required: true, maxlength: 5000 },
 			ddlEmailType: { required: true },
 		},
 		errorPlacement: function (error, element) {
@@ -15,3 +14,15 @@ $(function () {
 		},
 	});
 });
+
+function setupSummerNote() {
+	$('#txtBody').summernote({
+		fontNames: ['Open Sans'],
+		fontName: 'Open Sans',
+	});
+
+	$('.note-editor .note-editable').css({
+		'line-height': '0.8',
+		'font-family': '"Open Sans", sans-serif',
+	});
+}
