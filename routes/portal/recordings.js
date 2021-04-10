427492eab0b4be467e61062219ad1171aa3ea258
Michaela Matocha <mich.a.matocha@gmail.com>
2021-04-10 12:47:35 -0500

    Added recordings page

---
+++ routes/portal/recordings.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const flash = require('express-flash');
+const sql = require('mssql');
+const middleware = require('../../middleware');
+const moment = require('moment');
+const tz = require('moment-timezone');
+
+router.get('/', middleware.checkAuthenticated, function (req, res) {
+	var sqlQuery =
+		'SELECT e.* FROM tbl_events e WHERE rec_url <> \'\'';
+
+	var sqlReq = new sql.Request()
+		.query(sqlQuery)
+		.then((result) => {
+			res.render('portal/recordings/index', { events: result.recordset });
+		})
+		.catch((err) => {
+			req.flash('error', 'Error loading events.');
+			res.render('portal/recordings/index', { events: [] });
+		});
+});
+
+module.exports = router;
