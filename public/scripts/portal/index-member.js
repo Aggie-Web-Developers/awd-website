830e7146f70be5f7e2b17c09acf1043ba0c346e1
Cole D. Connelly <cdconn00@gmail.com>
2021-06-22 13:14:59 -0500

    Remove javascript logic, create example a tag.

--- public/scripts/portal/index-member.js
+++ public/scripts/portal/index-member.js
@@ -1,19 +1,1 @@
 // JS File for Portal Index View
-
-$(function () {
-	$('#divEvents').click(function () {
-		window.location.href = '/portal/events';
-	});
-
-	$('#divRecords').click(function () {
-		window.location.href = '/portal/recordings';
-	});
-
-	$('#divViewUserProfile').click(function () {
-		window.location.href = '/portal/profile';
-	});
-
-	$('#divEditUserProfile').click(function () {
-		window.location.href = 'portal/profile/edit';
-	});
-});
