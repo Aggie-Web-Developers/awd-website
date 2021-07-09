af550d9a4ec601ba9680c3c06b0fa70cc8f6c203
Cole D. Connelly <cdconn00@gmail.com>
2021-07-09 07:36:22 -0500

    Cleanup comments, whitespace.

--- routes/portal/officers.js
+++ routes/portal/officers.js
@@ -6,15 +6,13 @@ const middleware = require('../../middleware');
 
 router.get('/', middleware.checkIsOfficer, function (req, res) {
 	var sqlQuery =
-		'SELECT o.*,u.id AS user_id, CONCAT(u.first_name, \' \', u.last_name) AS user_name ' +
-        'FROM tbl_officer_positions o ' +
-        'LEFT JOIN tbl_user u ON o.id = u.officer_id ';
+		"SELECT o.*,u.id AS user_id, CONCAT(u.first_name, ' ', u.last_name) AS user_name " +
+		'FROM tbl_officer_positions o ' +
+		'LEFT JOIN tbl_user u ON o.id = u.officer_id ';
 
 	var sqlReq = new sql.Request()
 		.query(sqlQuery)
 		.then((result) => {
-            //console.log(result.recordset);
-            //res.send("OK");
 			res.render('portal/officers/index', { officers: result.recordset });
 		})
 		.catch((err) => {
@@ -25,4 +23,3 @@ router.get('/', middleware.checkIsOfficer, function (req, res) {
 });
 
 module.exports = router;
-
