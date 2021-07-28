ba5f814badb5f7cabdfc78e997b6042af91621e2
Cole D. Connelly <cdconn00@gmail.com>
2021-07-27 19:08:48 -0500

    Complete officer edit logic.

--- routes/portal/officers.js
+++ routes/portal/officers.js
@@ -18,23 +18,50 @@ router.get('/', middleware.checkIsOfficer, function (req, res) {
 		.catch((err) => {
 			console.error(err);
 			req.flash('error', 'Error loading events.');
-			res.render('portal/officers/index', { events: [] });
+			res.render('portal/officers/index', { officers: [] });
 		});
 });
 
 router.get('/edit/:id', middleware.checkIsOfficer, function (req, res) {
 	var sqlQuery =
-		"SELECT id, CONCAT(first_name, ' ', last_name) AS name FROM tbl_user";
+		"SELECT u.id, CONCAT(u.first_name, ' ', u.last_name) AS name, " +
+		'(SELECT TOP 1 id FROM tbl_user WHERE officer_id = @id) as current_officer_user_id, ' +
+		'(SELECT TOP 1 name FROM tbl_officer_positions WHERE id = @id) as officer_position ' +
+		'FROM tbl_user u ';
 
-	var sqlReq = new sql.Request()
+	new sql.Request()
+		.input('id', sql.Int, req.params.id)
 		.query(sqlQuery)
 		.then((result) => {
-			res.render('portal/officers/edit', { users: result.recordset });
+			res.render('portal/officers/edit', {
+				users: result.recordset,
+				officer_id: req.params.id,
+			});
 		})
 		.catch((err) => {
 			console.error(err);
 			req.flash('error', 'Error loading events.');
-			res.render('portal/officers/index', { events: [] });
+			res.redirect('/portal/officers/');
+		});
+});
+
+router.put('/edit/:id', middleware.checkIsOfficer, function (req, res) {
+	var sqlQuery =
+		'UPDATE tbl_user SET officer_id = NULL WHERE officer_id = @officer_id;' +
+		'UPDATE tbl_user SET officer_id = @officer_id WHERE id = @user_id;';
+
+	new sql.Request()
+		.input('officer_id', sql.Int, req.params.id)
+		.input('user_id', sql.Int, req.body.ddlUser)
+		.query(sqlQuery)
+		.then((result) => {
+			req.flash('success', 'Success! Officer updated.');
+			res.redirect('/portal/officers');
+		})
+		.catch((err) => {
+			console.error(err);
+			req.flash('error', 'Error loading events.');
+			res.redirect('/portal/officers/');
 		});
 });
 
