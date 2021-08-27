3350bef1ea6dc9e82bde537aa6a6c384de9d538f
Cole D. Connelly <cdconn00@gmail.com>
2021-08-27 17:30:17 -0500

    Comment out register routes.

--- routes/portal/index.js
+++ routes/portal/index.js
@@ -30,76 +30,76 @@ router.post(
 	})
 );
 
-router.get('/register', middleware.checkNotAuthenticated, function (req, res) {
-	res.render('portal/register');
-});
+// router.get('/register', middleware.checkNotAuthenticated, function (req, res) {
+// 	res.render('portal/register');
+// });
 
-router.post('/register', middleware.checkNotAuthenticated, async function (
-	req,
-	res
-) {
-	try {
-		const hashedPassword = await bcrypt.hash(req.body.txtPassword, 10);
-		const activationId = uuidv4();
-		const siteType =
-			process.env.NODE_ENV == 'prod' ? 'aggiedevelopers.com' : 'localhost:8080';
-		const link = 'http://' + siteType + '/portal/activate/' + activationId;
+// router.post('/register', middleware.checkNotAuthenticated, async function (
+// 	req,
+// 	res
+// ) {
+// 	try {
+// 		const hashedPassword = await bcrypt.hash(req.body.txtPassword, 10);
+// 		const activationId = uuidv4();
+// 		const siteType =
+// 			process.env.NODE_ENV == 'prod' ? 'aggiedevelopers.com' : 'localhost:8080';
+// 		const link = 'http://' + siteType + '/portal/activate/' + activationId;
 
-		var sqlReq = new sql.Request();
-		sqlReq.input('first_name', sql.NVarChar, req.body.txtFirstName);
-		sqlReq.input('last_name', sql.NVarChar, req.body.txtLastName);
-		sqlReq.input('email', sql.NVarChar, req.body.txtEmailAddress);
-		sqlReq.input('password_hash', sql.NVarChar, hashedPassword);
-		sqlReq.input('receiveNewsletter', sql.Bit, req.body.chkNews === 'on');
-		sqlReq.input('activation_id', sql.NVarChar, activationId);
+// 		var sqlReq = new sql.Request();
+// 		sqlReq.input('first_name', sql.NVarChar, req.body.txtFirstName);
+// 		sqlReq.input('last_name', sql.NVarChar, req.body.txtLastName);
+// 		sqlReq.input('email', sql.NVarChar, req.body.txtEmailAddress);
+// 		sqlReq.input('password_hash', sql.NVarChar, hashedPassword);
+// 		sqlReq.input('receiveNewsletter', sql.Bit, req.body.chkNews === 'on');
+// 		sqlReq.input('activation_id', sql.NVarChar, activationId);
 
-		var queryText =
-			'IF NOT EXISTS (SELECT * FROM tbl_user WHERE email = @email) ' +
-			'BEGIN ' +
-			'INSERT INTO tbl_user (first_name, last_name, email, password_hash, receiveNewsletter, activation_id) ' +
-			'values (@first_name, @last_name, @email, @password_hash, @receiveNewsletter, @activation_id) ' +
-			'END';
+// 		var queryText =
+// 			'IF NOT EXISTS (SELECT * FROM tbl_user WHERE email = @email) ' +
+// 			'BEGIN ' +
+// 			'INSERT INTO tbl_user (first_name, last_name, email, password_hash, receiveNewsletter, activation_id) ' +
+// 			'values (@first_name, @last_name, @email, @password_hash, @receiveNewsletter, @activation_id) ' +
+// 			'END';
 
-		sqlReq
-			.query(queryText)
-			.then((result) => {
-				if (result.rowsAffected == 0) {
-					req.flash(
-						'error',
-						'Error creating account. Your email address in use.'
-					);
-					res.redirect('/portal/register');
-				} else {
-					email.sendValidationEmail(
-						req.body.txtFirstName,
-						req.body.txtEmailAddress,
-						link
-					);
+// 		sqlReq
+// 			.query(queryText)
+// 			.then((result) => {
+// 				if (result.rowsAffected == 0) {
+// 					req.flash(
+// 						'error',
+// 						'Error creating account. Your email address in use.'
+// 					);
+// 					res.redirect('/portal/register');
+// 				} else {
+// 					email.sendValidationEmail(
+// 						req.body.txtFirstName,
+// 						req.body.txtEmailAddress,
+// 						link
+// 					);
 
-					req.flash(
-						'success',
-						'Account created! Please verify your email to login.'
-					);
-					res.redirect('/portal/login');
-				}
-			})
-			.catch((err) => {
-				console.error(err);
+// 					req.flash(
+// 						'success',
+// 						'Account created! Please verify your email to login.'
+// 					);
+// 					res.redirect('/portal/login');
+// 				}
+// 			})
+// 			.catch((err) => {
+// 				console.error(err);
 
-				req.flash(
-					'error',
-					'Error creating account. Please contact us if the error persists.'
-				);
-				res.redirect('/portal/register');
-			});
-	} catch {
-		req.flash(
-			'error',
-			'Error creating account. Please contact us if the error persists.'
-		);
-		res.redirect('/portal/register');
-	}
-});
+// 				req.flash(
+// 					'error',
+// 					'Error creating account. Please contact us if the error persists.'
+// 				);
+// 				res.redirect('/portal/register');
+// 			});
+// 	} catch {
+// 		req.flash(
+// 			'error',
+// 			'Error creating account. Please contact us if the error persists.'
+// 		);
+// 		res.redirect('/portal/register');
+// 	}
+// });
 
 router.get('/activate/:id', function (req, res) {
 	const uuidv4 = new RegExp(
