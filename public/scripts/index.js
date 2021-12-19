91e6f85310373acc2579227263685bc8e70c5ce2
NkemdiAnyiam <NkemdiAnyiam@gmail.com>
2021-12-18 20:12:31 -0600

    Improve carousel functionality

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -93,208 +93,164 @@ const wait = milliseconds =>
 /* ************************************** */
 /* *************TESTIMONIALS************* */
 /* ************************************** */
-// Example set of testimonial data
-const testimonials = [
-	{
-		quote: `So many fun uses! I'm planning on using this playsilk for newborn photos for my baby girl, who\
-	is due any day now! Definitely telling everyone I know about this shop.`,
-		imgUrl: `images/Trey-Bio-Pic-min.jpg`,
-		fullName: `Captain America`,
-		year: 22,
-		position: `Member`
-	},
-	{
-		quote: `I don't feel like grabbing filler text here.`,
-		imgUrl: `images/Vignesh-Bio-Pic-min.jpg`,
-		fullName: `Hawkeye`,
-		year: 22,
-		position: `Operations Officer`
-	},
-	{
-		quote: `
-	Tincidunt lobortis feugiat vivamus at augue eget arcu. Consectetur libero id faucibus nisl
-	tincidunt eget. Dignissim sodales ut eu sem integer vitae justo.
-`,
-		imgUrl: `images/Cole-Bio-Pic-min.jpg`,
-		fullName: `Tony Stark`,
-		year: 22,
-		position: `Former Officer`
-	},
-	{
-		quote: `
-		Ipsum dolor sit amet consectetur adipiscing. Est pellentesque elit ullamcorper dignissim cras
-		tincidunt lobortis feugiat vivamus. Sit amet nulla facilisi morbi tempus iaculis urna id
-		volutpat.
+// IIFE
+(function() {
+	// Example set of testimonial data
+	const testimonials = [
+		{
+			quote: `So many fun uses! I'm planning on using this playsilk for newborn photos for my baby girl, who\
+		is due any day now! Definitely telling everyone I know about this shop.`,
+			imgUrl: `images/Trey-Bio-Pic-min.jpg`,
+			fullName: `Captain America`,
+			year: 22,
+			position: `Member`
+		},
+		{
+			quote: `I don't feel like grabbing filler text here.`,
+			imgUrl: `images/Vignesh-Bio-Pic-min.jpg`,
+			fullName: `Hawkeye`,
+			year: 22,
+			position: `Operations Officer`
+		},
+		{
+			quote: `
+		Tincidunt lobortis feugiat vivamus at augue eget arcu. Consectetur libero id faucibus nisl
+		tincidunt eget. Dignissim sodales ut eu sem integer vitae justo.
 	`,
-		imgUrl: `images/Alan-Bio-Pic-min.jpg`,
-		fullName: `Peter Parker`,
-		year: 22,
-		position: `Project Manager`
-	},
-];
+			imgUrl: `images/Cole-Bio-Pic-min.jpg`,
+			fullName: `Tony Stark`,
+			year: 22,
+			position: `Former Officer`
+		},
+		{
+			quote: `
+			Ipsum dolor sit amet consectetur adipiscing. Est pellentesque elit ullamcorper dignissim cras
+			tincidunt lobortis feugiat vivamus. Sit amet nulla facilisi morbi tempus iaculis urna id
+			volutpat.
+		`,
+			imgUrl: `images/Alan-Bio-Pic-min.jpg`,
+			fullName: `Peter Parker`,
+			year: 22,
+			position: `Project Manager`
+		},
+	];
 
-let startIdx = 0; // index into the above array of testimonial data
+	// array of testimonial card Elements
+	const testimonialCards = [
+	];
 
-function setTestimonialCard({quote, imgUrl, fullName, year, position}, cardNum) {
-	const testimonialCard = document.querySelector(`.testimonial-card:nth-child(${cardNum})`);
-	testimonialCard.innerHTML = `
-	<div class="testimonial-card__body">
-		<div class="testimonial-card__quotation-mark-container">
-			<img class="testimonial-card__quotation-mark" src="images/SVGs/opening-quotation-mark.svg"
-				alt="opening quotation mark" />
+	// populate testimonialCards
+	testimonials.forEach(({quote, imgUrl, fullName, year, position}) => {
+		const el = document.createElement('div');
+		el.classList.add('testimonial-card');
+		el.innerHTML = `
+		<div class="testimonial-card__body">
+			<div class="testimonial-card__quotation-mark-container">
+				<img class="testimonial-card__quotation-mark" src="images/SVGs/opening-quotation-mark.svg"
+					alt="opening quotation mark" />
+			</div>
+			<p class="testimonial-card__quote paragraph">
+				${quote}
+			</p>
 		</div>
-		<p class="testimonial-card__quote paragraph">
-			${quote}
-		</p>
-	</div>
-	<div class="testimonial-card__footer">
-		<div class="photo-small">
-			<img class="photo-small__img" src="${imgUrl}" alt="${fullName}">
+		<div class="testimonial-card__footer">
+			<div class="photo-small">
+				<img class="photo-small__img" src="${imgUrl}" alt="${fullName}">
+			</div>
+			<div class="testimonial-card__member-details">
+				<p class="testimonial-card__member-name">${fullName} &lsquo;${year}</p>
+				<p class="testimonial-card__member-position">${position}</p>
+			</div>
 		</div>
-		<div class="testimonial-card__member-details">
-			<p class="testimonial-card__member-name">${fullName} &lsquo;${year}</p>
-			<p class="testimonial-card__member-position">${position}</p>
-		</div>
-	</div>
-	`;
-};
+		`;
+		testimonialCards.push(el);
+	});
 
-// set all 3 testimonial cards
-function setTestimonialCardAll(testimonials, startIdx) {
-	setTestimonialCard(testimonials[startIdx], 1);
-	setTestimonialCard(testimonials[(startIdx+1) % testimonials.length], 2);
-	setTestimonialCard(testimonials[(startIdx+2) % testimonials.length], 3);
-}
-
-// initialize the testimonial cards
-setTestimonialCardAll(testimonials, 0);
-
-// carousel control buttons
-const carouselControlLeft = document.querySelector('.carousel-control--left');
-const carouselControlRight = document.querySelector('.carousel-control--right');
-let leftClicked = false;
-let rightClicked = false;
-
-carouselControlLeft.addEventListener('click', function() {
-	if (leftClicked) { return; }
-	leftClicked = true;
-	startIdx = startIdx > 0 ? startIdx - 1 : testimonials.length - 1;
-	const testimonialCard1 = document.querySelector(`.testimonial-card:nth-child(1)`);
-	const testimonialCard2 = document.querySelector(`.testimonial-card:nth-child(2)`);
-	const testimonialCard3 = document.querySelector(`.testimonial-card:nth-child(3)`);
-	const viewportWidth = window.innerWidth;
-	if (viewportWidth > 1024) {
-		testimonialCard1.classList.add('moveRight');
-		testimonialCard2.classList.add('moveRight');
-		testimonialCard3.classList.add('leaveRight');
-
-		wait(300)
-			.then(() => {
-				testimonialCard1.classList.add('enterLeft');
-				setTestimonialCardAll(testimonials, startIdx);
-				return wait(100);
-			})
-			.then(() => {
-				testimonialCard1.classList.remove('moveRight');
-				testimonialCard2.classList.remove('moveRight');
-				testimonialCard3.classList.remove('leaveRight');
-				testimonialCard1.classList.remove('enterLeft');
-				leftClicked = false;
-			});
+	// append first 3 elements of array to container of testimonial cards
+	const testimonialsContainer = document.querySelector('.testimonial-cards');
+	for (let i = 0; i < testimonialCards.length && i < 3; ++i) {
+		testimonialsContainer.append(testimonialCards[i]);
 	}
-	else if (viewportWidth > 768) {
-		testimonialCard1.classList.add('moveRight');
-		testimonialCard2.classList.add('leaveRight');
 
-		wait(300)
-			.then(() => {
-				setTestimonialCardAll(testimonials, startIdx);
-				testimonialCard1.classList.add('enterLeft');
-				return wait(300);
-			})
-			.then(() => {
-				testimonialCard1.classList.remove('moveRight');
-				testimonialCard2.classList.remove('leaveRight');
-				testimonialCard1.classList.remove('enterLeft');
-				leftClicked = false;
-			});
-	}
-	else  {
-		testimonialCard1.classList.add('leaveRight');
+	// carousel control buttons
+	const carouselControlLeft = document.querySelector('.carousel-control--left');
+	const carouselControlRight = document.querySelector('.carousel-control--right');
+	let leftClicked = false;
+	let rightClicked = false;
 
-		wait(300)
-			.then(() => {
-				setTestimonialCardAll(testimonials, startIdx);
-				testimonialCard1.classList.add('enterLeft');
-				return wait(300);
-			})
-			.then(() => {
-				testimonialCard1.classList.remove('leaveRight');
-				testimonialCard1.classList.remove('enterLeft');
-				leftClicked = false;
-			});
-	}
-});
+	carouselControlLeft.addEventListener('click', async function() {
+		if (leftClicked) { return; }
+		leftClicked = true;
 
-carouselControlRight.addEventListener('click', function() {
-	if (rightClicked) { return; }
-	rightClicked = true;
-	startIdx = (startIdx + 1) % testimonials.length;
+		// using viewportWidth and idx to account for media queries (because number of displayed cards will vary)
+		const viewportWidth = window.innerWidth;
+		let idx;
+		if (viewportWidth > 1400) { idx = 2; }
+		else if (viewportWidth > 1024) { idx = 1; }
+		else { idx = 0; }
+		idx = Math.min(idx, testimonialCards.length - 1);
 
-	const testimonialCard1 = document.querySelector(`.testimonial-card:nth-child(1)`);
-	const testimonialCard2 = document.querySelector(`.testimonial-card:nth-child(2)`);
-	const testimonialCard3 = document.querySelector(`.testimonial-card:nth-child(3)`);
-	const viewportWidth = window.innerWidth;
+		// leftmost cards move right
+		for (let i = 0; i < idx; ++i) { testimonialCards[i].classList.add('moveRight'); }
+		// rightmost card leaves to the right
+		testimonialCards[idx].classList.add('leaveRight');
+		
+		// wait for moving/leaving animations to end
+		await wait(200);
 
-	if (viewportWidth > 1024) {
-		testimonialCard3.classList.add('moveLeft');
-		testimonialCard2.classList.add('moveLeft');
-		testimonialCard1.classList.add('leaveLeft');
+		// remove the last child from the container of cards
+		testimonialsContainer.removeChild(testimonialsContainer.querySelector('.testimonial-card:last-child'));
+		// insert the last element from the array of cards to the beginning of the container. This should be intuitive.
+		testimonialsContainer.insertAdjacentElement('afterbegin', testimonialCards[testimonialCards.length - 1]);
+		// remove animation classes from cards to prevent odd graphical glitch
+		for (let i = 0; i < idx; ++i) { testimonialCards[i].className = 'testimonial-card'; }
+		// animate new card entering from the left
+		testimonialCards[testimonialCards.length - 1].classList.add('enterLeft');
 
-		wait(300)
-			.then(() => {
-				testimonialCard3.classList.add('enterRight');
-				setTestimonialCardAll(testimonials, startIdx);
-				return wait(300);
-			})
-			.then(() => {
-				testimonialCard3.classList.remove('moveLeft');
-				testimonialCard2.classList.remove('moveLeft');
-				testimonialCard1.classList.remove('leaveLeft');
-				testimonialCard3.classList.remove('enterRight');
-				rightClicked = false;
-			});
-	}
-	else if (viewportWidth > 768) {
-		testimonialCard2.classList.add('moveLeft');
-		testimonialCard1.classList.add('leaveLeft');
+		// wait for entering animation to finish
+		await wait(100);
 
-		wait(300)
-			.then(() => {
-				setTestimonialCardAll(testimonials, startIdx);
-				testimonialCard2.classList.add('enterRight');
-				return wait(300);
-			})
-			.then(() => {
-				testimonialCard2.classList.remove('moveLeft');
-				testimonialCard1.classList.remove('leaveLeft');
-				testimonialCard2.classList.remove('enterRight');
-				rightClicked = false;
-			});
-	}
-	else  {
-		testimonialCard1.classList.add('leaveLeft');
+		// remove all animations
+		for (let i = 0; i < idx; ++i) { testimonialCards[i].classList.remove('moveRight'); }
+		testimonialCards[idx].classList.remove('leaveRight');
+		testimonialCards[testimonialCards.length - 1].classList.remove('enterLeft');
 
-		wait(300)
-			.then(() => {
-				setTestimonialCardAll(testimonials, startIdx);
-				testimonialCard1.classList.add('enterRight');
-				return wait(300);
-			})
-			.then(() => {
-				testimonialCard1.classList.remove('leaveLeft');
-				testimonialCard1.classList.remove('enterRight');
-				rightClicked = false;
-			});
-	}
-});
+		// shift the array of cards to the right (wrapping) to match new orientation
+		testimonialCards.unshift(testimonialCards.pop());
+
+		leftClicked = false;
+	});
+
+	carouselControlRight.addEventListener('click', async function() {
+		if (rightClicked) { return; }
+		rightClicked = true;
+
+		const viewportWidth = window.innerWidth;
+		let idx;
+		if (viewportWidth > 1400) { idx = 2; }
+		else if (viewportWidth > 1024) { idx = 1; }
+		else { idx = 0; }
+		idx = Math.min(idx, testimonialCards.length - 1);
+
+		for (let i = idx; i > 0; --i) { testimonialCards[i].classList.add('moveLeft'); }
+		testimonialCards[0].classList.add('leaveLeft');
+
+		await wait(200);
+
+		testimonialsContainer.removeChild(testimonialsContainer.querySelector('.testimonial-card:first-child'));
+		testimonialsContainer.insertAdjacentElement('beforeend', testimonialCards[testimonialCards.length - 1]);
+		for (let i = idx; i > 0; --i) { testimonialCards[i].className = 'testimonial-card'; }
+		testimonialCards[(idx + 1) % testimonialCards.length].classList.add('enterRight');
+
+		await wait(100);
+
+		for (let i = idx; i > 0; --i) { testimonialCards[i].classList.remove('moveLeft'); }
+		testimonialCards[0].classList.remove('leaveLeft');
+		testimonialCards[(idx + 1) % testimonialCards.length].classList.remove('enterRight');
+
+		testimonialCards.push(testimonialCards.shift());
+
+		rightClicked = false;
+	});
+})();
+
