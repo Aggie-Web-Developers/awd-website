a5862641a5033e60931666ef67f289d1c340218f
NkemdiAnyiam <NkemdiAnyiam@gmail.com>
2021-12-21 23:05:20 -0600

    Create Carousel class

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -173,198 +173,233 @@ const wait = milliseconds =>
 		testimonialsContainer.append(testimonialCards[i]);
 	}
 
+	class Carousel {
+		_maxItemsDisplayed = 3; // hard coded for now
+		_currFrontIdx = 0;
+		_indicatorsArray = [];
+		_transitionTime1 = 200; // hard coded for now
+		_transitionTime2 = 100; // hard coded for now
+		_transitionTimeFast = 25; // hard coded for now
+		_transitioning = false;
 
+		constructor({carouselID, itemsContainerClass, itemElemsArray, numItems, breakpoint1, breakpoint2}) {
+			this._carouselElem = document.getElementById(`${carouselID}`);
+			this._itemElemsArray = itemElemsArray;
+			this._numItems = numItems;
+			this._itemsContainerElem = this._carouselElem.querySelector(`.${itemsContainerClass}`);
+			this._breakpoint1 = breakpoint1;
+			this._breakpoint2 = breakpoint2;
+			
 
-	// CAROUSEL FIELDS
-	const carouselControlLeft = document.querySelector('.carousel__control--left');
-	const carouselControlRight = document.querySelector('.carousel__control--right');
-	const numItems = testimonialCards.length;
-	let currFrontIdx = 0; // index of the leftmost item in the carousel
-	const breakpoint1 = 1400; // wider breakpoint
-	const breakpoint2 = 1024; // thinner breakpoint
-	let transitioning = false;
-	const transitionTime1 = 200; // duration of first transition in milliseconds
-	const transitionTime2 = 100;
-	const transitionTimeFast = 25; // used for indicator presses
-
-	// array of carousel indicators
-	const carouselIndicators = [
-	];
-
-	// create indicators
-	for (let i = 0; i < numItems; ++i) {
-		const newIndicator = document.createElement('div');
-		newIndicator.classList.add('carousel__indicator');
-		newIndicator.dataset.index = `${i}`;
-
-		carouselIndicators.push(newIndicator);
-	}
-
-	// append all carousel indicators to container of indicators
-	const indicatorsContainer = document.querySelector('.carousel__indicators');
-	for (let i = 0; i < carouselIndicators.length; ++i) {
-		indicatorsContainer.append(carouselIndicators[i]);
-	}
-
-	// set first indicator to be active
-	carouselIndicators[0].classList.add('active');
-
-	function setIndicator(currIdx, targetIdx) {
-		carouselIndicators[currIdx].classList.remove('active');
-		carouselIndicators[targetIdx].classList.add('active');
-
-		return targetIdx;
-	}
-
-	// get distance of rightmost item in carousel (number will vary because of media queries based on viewport width)
-	function getRightmostDist() {
-		let rightmostDist; // distance between leftmost item and rightmost item
-		if (window.innerWidth > breakpoint1) { rightmostDist = 2; }
-		else if (window.innerWidth > breakpoint2) { rightmostDist = 1; }
-		else { rightmostDist = 0; }
-
-		// accounts for carousel with less than 3 items
-		rightmostDist = Math.min(rightmostDist, numItems - 1);
-
-		return rightmostDist;
-	}
-
-	// move left in carousel
-	async function shiftLeft(isFast) {
-		if (transitioning) { return; }
-		transitioning = true;
-
-		const rightmostDist = getRightmostDist();
-
-		// leftmost items move right
-		for (let i = 0; i < rightmostDist; ++i) {
-			isFast && testimonialCards[i].classList.add('transition-fast');
-			testimonialCards[i].classList.add('moveRight');
+			this._indicatorsContainerElem = this._carouselElem.querySelector('.carousel__indicators');
+			this._controlLeft = this._carouselElem.querySelector('.carousel__control--left');
+			this._controlRight = this._carouselElem.querySelector('.carousel__control--right');
 		}
-		// rightmost item leaves to the right
-		isFast && testimonialCards[rightmostDist].classList.add('transition-fast');
-		testimonialCards[rightmostDist].classList.add('leaveRight');
 
-		// wait for moving/leaving animations to end
-		await wait(isFast ? transitionTimeFast : transitionTime1);
-
-		// remove the last child from the container of items
-		testimonialsContainer.removeChild(testimonialsContainer.querySelector('.testimonial-card:last-child'));
-		// insert the last element from the array of items to the beginning of the container. This should be intuitive.
-		testimonialsContainer.insertAdjacentElement('afterbegin', testimonialCards[numItems - 1]);
-
-		// remove animation classes from items to prevent odd graphical glitch
-		for (let i = 0; i < rightmostDist; ++i) { testimonialCards[i].className = 'testimonial-card'; }
-		// animate new item entering from the left
-		isFast && testimonialCards[numItems - 1].classList.add('transition-fast');
-		testimonialCards[numItems - 1].classList.add('enterLeft');
-
-		// wait for entering animation to finish
-		await wait(isFast ? transitionTimeFast : transitionTime2);
-
-		// remove all animations
-		for (let i = 0; i < rightmostDist; ++i) { testimonialCards[i].classList.remove('moveRight', 'transition-fast'); }
-		testimonialCards[rightmostDist].classList.remove('leaveRight', 'transition-fast');
-		testimonialCards[numItems - 1].classList.remove('enterLeft', 'transition-fast');
-
-		// shift the array of items to the right (wrapping) to match new orientation
-		testimonialCards.unshift(testimonialCards.pop());
-
-		transitioning = false;
-	}
-
-	// move right in carousel
-	async function moveRight(isFast) {
-		// NOTE: upon first glance, it may seem that this could could easily be refactored from moveLeft. That is not the case
-		if (transitioning) { return; }
-		transitioning = true;
-
-		const rightmostDist = getRightmostDist();
-
-		for (let i = rightmostDist; i > 0; --i) {
-			isFast && testimonialCards[i].classList.add('transition-fast');
-			testimonialCards[i].classList.add('moveLeft');
+		initialize() {
+			this._fillItemsContainer();
+			this._createIndicators();
+			this._attachCarouselBtnListeners();
 		}
-		isFast && testimonialCards[0].classList.add('transition-fast');
-		testimonialCards[0].classList.add('leaveLeft');
 
-		await wait(isFast ? transitionTimeFast : transitionTime1);
-
-		testimonialsContainer.removeChild(testimonialsContainer.querySelector('.testimonial-card:first-child'));
-		testimonialsContainer.insertAdjacentElement('beforeend', testimonialCards[numItems - 1]);
-
-		for (let i = rightmostDist; i > 0; --i) { testimonialCards[i].className = 'testimonial-card'; }
-		isFast && testimonialCards[(rightmostDist + 1) % numItems].classList.add('transition-fast');
-		testimonialCards[(rightmostDist + 1) % numItems].classList.add('enterRight');
-
-		await wait(isFast ? transitionTimeFast : transitionTime2);
-
-		for (let i = rightmostDist; i > 0; --i) { testimonialCards[i].classList.remove('moveLeft', 'transition-fast'); }
-		testimonialCards[0].classList.remove('leaveLeft', 'transition-fast');
-		testimonialCards[(rightmostDist + 1) % numItems].classList.remove('enterRight', 'transition-fast');
-
-		testimonialCards.push(testimonialCards.shift());
-
-		transitioning = false;
-	}
-
-	// handle clicking left carousel control
-	carouselControlLeft.addEventListener('click', function() {
-		shiftLeft();
-		currFrontIdx = setIndicator(currFrontIdx, currFrontIdx - 1 >= 0 ? currFrontIdx - 1 : numItems - 1);
-	});
-
-	// handle clicking right carousel control
-	carouselControlRight.addEventListener('click', async function() {
-		moveRight();
-		currFrontIdx = setIndicator(currFrontIdx, (currFrontIdx + 1) % numItems);
-	});
-
-	// handle clicking an indicator
-	indicatorsContainer.addEventListener('click', async function(e) {
-		const clicked = e.target.closest('.carousel__indicator');
-
-		if (!clicked || clicked.classList.contains('active')) { return; }
-
-		const prevFrontIdx = currFrontIdx;
-		const targetIdx = parseInt(clicked.dataset.index);
-		currFrontIdx = setIndicator(currFrontIdx, targetIdx);
-
-		// get distances for travelling left or right to reach target item
-		const leftDist = targetIdx > prevFrontIdx
-			? numItems - (targetIdx - prevFrontIdx)
-			: Math.abs(targetIdx - prevFrontIdx);
-		const rightDist = targetIdx > prevFrontIdx
-			? targetIdx - prevFrontIdx
-			: numItems - Math.abs(targetIdx - prevFrontIdx);
-
-		// if left distance is shorter, use left shifts
-		if (leftDist < rightDist) {
-			for (let i = prevFrontIdx; i != targetIdx; i = (i - 1 >= 0 ? i - 1 : numItems - 1)) {
-				await shiftLeft(true);
+		_fillItemsContainer() {
+			for (let i = 0; i < this._numItems && i < this._maxItemsDisplayed; ++i) {
+				this._itemsContainerElem.append(this._itemElemsArray[i]);
 			}
 		}
-		// if right distance is shorter, use right shifts
-		else if (leftDist > rightDist) {
-			for (let i = prevFrontIdx; i != targetIdx; i = (i + 1) % numItems) {
-				await moveRight(true);
+
+		_createIndicators() {
+			for (let i = 0; i < this._numItems; ++i) {
+				// create indicator element
+				const newIndicator = document.createElement('div');
+				newIndicator.classList.add('carousel__indicator');
+				newIndicator.dataset.index = `${i}`;
+
+				// push to array of indicator elements
+				this._indicatorsArray.push(newIndicator);
+				// append to container of indicator elements
+				this._indicatorsContainerElem.append(newIndicator);
 			}
+			
+			// set first indicator to be active
+			this._indicatorsArray[0].classList.add('active');
 		}
-		// if distances are equivalent, move in direction of target relative to current
-		else {
-			// if target is to the left, use left shifts
-			if (targetIdx < prevFrontIdx) {
-				for (let i = prevFrontIdx; i > targetIdx; --i) {
-					await shiftLeft(true);
+
+		_setIndicator(currIdx, targetIdx) {
+			this._indicatorsArray[currIdx].classList.remove('active');
+			this._indicatorsArray[targetIdx].classList.add('active');
+
+			return targetIdx;
+		}
+
+		// gets distance of rightmost item in carousel (number will vary because of media queries based on viewport width)
+		_getRightmostDist() {
+			let rightmostDist; // distance between leftmost item and rightmost item
+			if (window.innerWidth > this._breakpoint1) { rightmostDist = 2; }
+			else if (window.innerWidth > this._breakpoint2) { rightmostDist = 1; }
+			else { rightmostDist = 0; }
+
+			// accounts for carousel with less than 3 items
+			rightmostDist = Math.min(rightmostDist, this._numItems - 1);
+
+			return rightmostDist;
+		}
+
+		// move left in carousel
+		async _shiftLeft(isFast) {
+			if (this._transitioning) { return; }
+			this._transitioning = true;
+	
+			const rightmostDist = this._getRightmostDist();
+	
+			// leftmost items move right
+			for (let i = 0; i < rightmostDist; ++i) {
+				isFast && this._itemElemsArray[i].classList.add('transition-fast');
+				this._itemElemsArray[i].classList.add('moveRight');
+			}
+			// rightmost item leaves to the right
+			isFast && this._itemElemsArray[rightmostDist].classList.add('transition-fast');
+			this._itemElemsArray[rightmostDist].classList.add('leaveRight');
+	
+			// wait for moving/leaving animations to end
+			await wait(isFast ? this._transitionTimeFast : this._transitionTime1);
+	
+			// remove the last child from the container of items
+			this._itemsContainerElem.removeChild(this._itemsContainerElem.querySelector('.testimonial-card:last-child'));
+			// insert the last element from the array of items to the beginning of the container. This should be intuitive.
+			this._itemsContainerElem.insertAdjacentElement('afterbegin', this._itemElemsArray[this._numItems - 1]);
+	
+			// remove animation classes from items to prevent odd graphical glitch
+			for (let i = 0; i < rightmostDist; ++i) { this._itemElemsArray[i].className = 'testimonial-card'; }
+			// animate new item entering from the left
+			isFast && this._itemElemsArray[this._numItems - 1].classList.add('transition-fast');
+			this._itemElemsArray[this._numItems - 1].classList.add('enterLeft');
+	
+			// wait for entering animation to finish
+			await wait(isFast ? this._transitionTimeFast : this._transitionTime2);
+	
+			// remove all animations
+			for (let i = 0; i < rightmostDist; ++i) { this._itemElemsArray[i].classList.remove('moveRight', 'transition-fast'); }
+			this._itemElemsArray[rightmostDist].classList.remove('leaveRight', 'transition-fast');
+			this._itemElemsArray[this._numItems - 1].classList.remove('enterLeft', 'transition-fast');
+	
+			// shift the array of items to the right (wrapping) to match new orientation
+			this._itemElemsArray.unshift(this._itemElemsArray.pop());
+	
+			this._transitioning = false;
+		}
+
+		// move right in carousel
+		async _shiftRight(isFast) {
+			// NOTE: upon first glance, it may seem that this could could easily be refactored from moveLeft. That is not the case
+			if (this._transitioning) { return; }
+			this._transitioning = true;
+
+			const rightmostDist = this._getRightmostDist();
+
+			for (let i = rightmostDist; i > 0; --i) {
+				isFast && this._itemElemsArray[i].classList.add('transition-fast');
+				this._itemElemsArray[i].classList.add('moveLeft');
+			}
+			isFast && this._itemElemsArray[0].classList.add('transition-fast');
+			this._itemElemsArray[0].classList.add('leaveLeft');
+
+			await wait(isFast ? this._transitionTimeFast : this._transitionTime1);
+
+			this._itemsContainerElem.removeChild(this._itemsContainerElem.querySelector('.testimonial-card:first-child'));
+			this._itemsContainerElem.insertAdjacentElement('beforeend', this._itemElemsArray[this._numItems - 1]);
+
+			for (let i = rightmostDist; i > 0; --i) { this._itemElemsArray[i].className = 'testimonial-card'; }
+			isFast && this._itemElemsArray[(rightmostDist + 1) % this._numItems].classList.add('transition-fast');
+			this._itemElemsArray[(rightmostDist + 1) % this._numItems].classList.add('enterRight');
+
+			await wait(isFast ? this._transitionTimeFast : this._transitionTime2);
+
+			for (let i = rightmostDist; i > 0; --i) { this._itemElemsArray[i].classList.remove('moveLeft', 'transition-fast'); }
+			this._itemElemsArray[0].classList.remove('leaveLeft', 'transition-fast');
+			this._itemElemsArray[(rightmostDist + 1) % this._numItems].classList.remove('enterRight', 'transition-fast');
+
+			this._itemElemsArray.push(this._itemElemsArray.shift());
+
+			this._transitioning = false;
+		}
+
+		_attachCarouselBtnListeners() {
+			// handle clicking left carousel control
+			const leftCallback = async function() {
+				await this._shiftLeft();
+				this._currFrontIdx = this._setIndicator(this._currFrontIdx, this._currFrontIdx - 1 >= 0
+					? this._currFrontIdx - 1
+					: this._numItems - 1);
+			};
+			this._controlLeft.addEventListener('click', leftCallback.bind(this));
+
+			// handle clicking right carousel control
+			const rightCallback = async function() {
+				await this._shiftRight();
+				this._currFrontIdx = this._setIndicator(this._currFrontIdx, (this._currFrontIdx + 1) % this._numItems);
+			};
+			this._controlRight.addEventListener('click', rightCallback.bind(this));
+
+			// handle clicking an indicator
+			const indicatorCallback = async function(e) {
+				const clicked = e.target.closest('.carousel__indicator');
+
+				if (!clicked || clicked.classList.contains('active')) { return; }
+
+				const prevFrontIdx = this._currFrontIdx;
+				const targetIdx = parseInt(clicked.dataset.index);
+				this._currFrontIdx = this._setIndicator(this._currFrontIdx, targetIdx);
+
+				// get distances for travelling left or right to reach target item
+				const leftDist = targetIdx > prevFrontIdx
+					? this._numItems - (targetIdx - prevFrontIdx)
+					: Math.abs(targetIdx - prevFrontIdx);
+				const rightDist = targetIdx > prevFrontIdx
+					? targetIdx - prevFrontIdx
+					: this._numItems - Math.abs(targetIdx - prevFrontIdx);
+
+				// if left distance is shorter, use left shifts
+				if (leftDist < rightDist) {
+					for (let i = prevFrontIdx; i != targetIdx; i = (i - 1 >= 0 ? i - 1 : this._numItems - 1)) {
+						await this._shiftLeft(true);
+					}
 				}
-			}
-			// if target is to the right, use right shifts
-			else {
-				for (let i = prevFrontIdx; i < targetIdx; ++i) {
-					await moveRight(true);
+				// if right distance is shorter, use right shifts
+				else if (leftDist > rightDist) {
+					for (let i = prevFrontIdx; i != targetIdx; i = (i + 1) % this._numItems) {
+						await this._shiftRight(true);
+					}
 				}
-			}
+				// if distances are equivalent, move in direction of target relative to current
+				else {
+					// if target is to the left, use left shifts
+					if (targetIdx < prevFrontIdx) {
+						for (let i = prevFrontIdx; i > targetIdx; --i) {
+							await this._shiftLeft(true);
+						}
+					}
+					// if target is to the right, use right shifts
+					else {
+						for (let i = prevFrontIdx; i < targetIdx; ++i) {
+							await this._shiftRight(true);
+						}
+					}
+				}
+			};
+			this._indicatorsContainerElem.addEventListener('click', indicatorCallback.bind(this));
 		}
-	});
+	};
+
+	const memberTestimonialsCarouselOptions = {
+		carouselID: 'member-testimonials-carousel',
+		itemsContainerClass: 'testimonial-cards',
+		itemElemsArray: testimonialCards,
+		numItems: testimonialCards.length,
+		breakpoint1: 1400,
+		breakpoint2: 1024,
+	};
+	const memberTestimonialsCarousel = new Carousel(memberTestimonialsCarouselOptions);
+	memberTestimonialsCarousel.initialize();
 })();
 
