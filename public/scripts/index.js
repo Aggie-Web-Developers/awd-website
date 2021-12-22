256b311a2d2d4c3c4b14283a1a80ac0d2a9b6c6c
NkemdiAnyiam <NkemdiAnyiam@gmail.com>
2021-12-22 00:54:02 -0600

    Create carousel for News And Events section and fix logical bugs in carousel

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -90,89 +90,8 @@ const wait = milliseconds =>
     );
 ;
 
-/* ************************************** */
-/* *************TESTIMONIALS************* */
-/* ************************************** */
 // IIFE
 (function() {
-	// Example set of testimonial data
-	const testimonials = [
-		{
-			quote: `So many fun uses! I'm planning on using this playsilk for newborn photos for my baby girl, who\
-		is due any day now! Definitely telling everyone I know about this shop.`,
-			imgUrl: `images/Trey-Bio-Pic-min.jpg`,
-			fullName: `Captain America`,
-			year: 22,
-			position: `Member`
-		},
-		{
-			quote: `I don't feel like grabbing filler text here.`,
-			imgUrl: `images/Vignesh-Bio-Pic-min.jpg`,
-			fullName: `Hawkeye`,
-			year: 22,
-			position: `Operations Officer`
-		},
-		{
-			quote: `
-		Tincidunt lobortis feugiat vivamus at augue eget arcu. Consectetur libero id faucibus nisl
-		tincidunt eget. Dignissim sodales ut eu sem integer vitae justo.
-	`,
-			imgUrl: `images/Cole-Bio-Pic-min.jpg`,
-			fullName: `Tony Stark`,
-			year: 22,
-			position: `Former Officer`
-		},
-		{
-			quote: `
-			Ipsum dolor sit amet consectetur adipiscing. Est pellentesque elit ullamcorper dignissim cras
-			tincidunt lobortis feugiat vivamus. Sit amet nulla facilisi morbi tempus iaculis urna id
-			volutpat.
-		`,
-			imgUrl: `images/Alan-Bio-Pic-min.jpg`,
-			fullName: `Peter Parker`,
-			year: 22,
-			position: `Project Manager`
-		},
-	];
-
-	// array of testimonial card Elements
-	const testimonialCards = [
-	];
-
-	// create testimonial cards
-	testimonials.forEach(({quote, imgUrl, fullName, year, position}, idx) => {
-		const newCard = document.createElement('div');
-		newCard.classList.add('testimonial-card');
-		newCard.innerHTML = `
-		<div class="testimonial-card__body">
-			<div class="testimonial-card__quotation-mark-container">
-				<img class="testimonial-card__quotation-mark" src="images/SVGs/opening-quotation-mark.svg"
-					alt="opening quotation mark" />
-			</div>
-			<p class="testimonial-card__quote paragraph">
-				${quote}
-			</p>
-		</div>
-		<div class="testimonial-card__footer">
-			<div class="photo-small">
-				<img class="photo-small__img" src="${imgUrl}" alt="${fullName}">
-			</div>
-			<div class="testimonial-card__member-details">
-				<p class="testimonial-card__member-name">${fullName} &lsquo;${year}</p>
-				<p class="testimonial-card__member-position">${position}</p>
-			</div>
-		</div>
-		`;
-
-		testimonialCards.push(newCard);
-	});
-
-	// append first 3 elements of array to container of testimonial cards
-	const testimonialsContainer = document.querySelector('.testimonial-cards');
-	for (let i = 0; i < testimonialCards.length && i < 3; ++i) {
-		testimonialsContainer.append(testimonialCards[i]);
-	}
-
 	class Carousel {
 		_maxItemsDisplayed = 3; // hard coded for now
 		_currFrontIdx = 0;
@@ -182,8 +101,9 @@ const wait = milliseconds =>
 		_transitionTimeFast = 25; // hard coded for now
 		_transitioning = false;
 
-		constructor({carouselID, itemsContainerClass, itemElemsArray, numItems, breakpoint1, breakpoint2}) {
+		constructor({carouselID, itemsContainerClass, itemElemClass, itemElemsArray, numItems, breakpoint1, breakpoint2}) {
 			this._carouselElem = document.getElementById(`${carouselID}`);
+			this._itemElemClass = itemElemClass;
 			this._itemElemsArray = itemElemsArray;
 			this._numItems = numItems;
 			this._itemsContainerElem = this._carouselElem.querySelector(`.${itemsContainerClass}`);
@@ -265,12 +185,12 @@ const wait = milliseconds =>
 			await wait(isFast ? this._transitionTimeFast : this._transitionTime1);
 	
 			// remove the last child from the container of items
-			this._itemsContainerElem.removeChild(this._itemsContainerElem.querySelector('.testimonial-card:last-child'));
+			this._itemsContainerElem.removeChild(this._itemsContainerElem.querySelector(`.${this._itemElemClass}:last-child`));
 			// insert the last element from the array of items to the beginning of the container. This should be intuitive.
 			this._itemsContainerElem.insertAdjacentElement('afterbegin', this._itemElemsArray[this._numItems - 1]);
 	
 			// remove animation classes from items to prevent odd graphical glitch
-			for (let i = 0; i < rightmostDist; ++i) { this._itemElemsArray[i].className = 'testimonial-card'; }
+			for (let i = 0; i < rightmostDist; ++i) { this._itemElemsArray[i].className = this._itemElemClass; }
 			// animate new item entering from the left
 			isFast && this._itemElemsArray[this._numItems - 1].classList.add('transition-fast');
 			this._itemElemsArray[this._numItems - 1].classList.add('enterLeft');
@@ -306,10 +226,11 @@ const wait = milliseconds =>
 
 			await wait(isFast ? this._transitionTimeFast : this._transitionTime1);
 
-			this._itemsContainerElem.removeChild(this._itemsContainerElem.querySelector('.testimonial-card:first-child'));
-			this._itemsContainerElem.insertAdjacentElement('beforeend', this._itemElemsArray[this._numItems - 1]);
+			this._itemsContainerElem.removeChild(this._itemsContainerElem.querySelector(`.${this._itemElemClass}:first-child`));
+			this._itemsContainerElem.insertAdjacentElement('beforeend',
+				this._itemElemsArray[Math.min(this._maxItemsDisplayed, this._numItems - 1)]);
 
-			for (let i = rightmostDist; i > 0; --i) { this._itemElemsArray[i].className = 'testimonial-card'; }
+			for (let i = rightmostDist; i > 0; --i) { this._itemElemsArray[i].className = this._itemElemClass; }
 			isFast && this._itemElemsArray[(rightmostDist + 1) % this._numItems].classList.add('transition-fast');
 			this._itemElemsArray[(rightmostDist + 1) % this._numItems].classList.add('enterRight');
 
@@ -390,10 +311,133 @@ const wait = milliseconds =>
 			this._indicatorsContainerElem.addEventListener('click', indicatorCallback.bind(this));
 		}
 	};
+	
+	/* ************************************** */
+	/* *************TESTIMONIALS************* */
+	/* ************************************** */
+	// Examples of testimonial data
+	const testimonials = [
+		{
+			quote: `So many fun uses! I'm planning on using this playsilk for newborn photos for my baby girl, who\
+		is due any day now! Definitely telling everyone I know about this shop.`,
+			imgUrl: `images/Trey-Bio-Pic-min.jpg`,
+			fullName: `Captain America`,
+			year: 22,
+			position: `Member`
+		},
+		{
+			quote: `So many fun uses! I'm planning on using this playsilk for newborn photos for my baby girl, who\
+		is due any day now! Definitely telling everyone I know about this shop.`,
+			imgUrl: `images/Trey-Bio-Pic-min.jpg`,
+			fullName: `Captain America`,
+			year: 22,
+			position: `A`
+		},
+		{
+			quote: `So many fun uses! I'm planning on using this playsilk for newborn photos for my baby girl, who\
+		is due any day now! Definitely telling everyone I know about this shop.`,
+			imgUrl: `images/Trey-Bio-Pic-min.jpg`,
+			fullName: `Captain America`,
+			year: 22,
+			position: `B`
+		},
+		{
+			quote: `So many fun uses! I'm planning on using this playsilk for newborn photos for my baby girl, who\
+		is due any day now! Definitely telling everyone I know about this shop.`,
+			imgUrl: `images/Trey-Bio-Pic-min.jpg`,
+			fullName: `Captain America`,
+			year: 22,
+			position: `C`
+		},
+		{
+			quote: `So many fun uses! I'm planning on using this playsilk for newborn photos for my baby girl, who\
+		is due any day now! Definitely telling everyone I know about this shop.`,
+			imgUrl: `images/Trey-Bio-Pic-min.jpg`,
+			fullName: `Captain America`,
+			year: 22,
+			position: `D`
+		},
+		{
+			quote: `So many fun uses! I'm planning on using this playsilk for newborn photos for my baby girl, who\
+		is due any day now! Definitely telling everyone I know about this shop.`,
+			imgUrl: `images/Trey-Bio-Pic-min.jpg`,
+			fullName: `Captain America`,
+			year: 22,
+			position: `E`
+		},
+		{
+			quote: `I don't feel like grabbing filler text here.`,
+			imgUrl: `images/Vignesh-Bio-Pic-min.jpg`,
+			fullName: `Hawkeye`,
+			year: 22,
+			position: `Operations Officer`
+		},
+		{
+			quote: `
+		Tincidunt lobortis feugiat vivamus at augue eget arcu. Consectetur libero id faucibus nisl
+		tincidunt eget. Dignissim sodales ut eu sem integer vitae justo.
+	`,
+			imgUrl: `images/Cole-Bio-Pic-min.jpg`,
+			fullName: `Tony Stark`,
+			year: 22,
+			position: `Former Officer`
+		},
+		{
+			quote: `
+			Ipsum dolor sit amet consectetur adipiscing. Est pellentesque elit ullamcorper dignissim cras
+			tincidunt lobortis feugiat vivamus. Sit amet nulla facilisi morbi tempus iaculis urna id
+			volutpat.
+		`,
+			imgUrl: `images/Alan-Bio-Pic-min.jpg`,
+			fullName: `Peter Parker`,
+			year: 22,
+			position: `Project Manager`
+		},
+	];
 
+	// array of testimonial card Elements
+	const testimonialCards = [
+	];
+
+	// create testimonial cards
+	testimonials.forEach(({quote, imgUrl, fullName, year, position}, idx) => {
+		const newCard = document.createElement('div');
+		newCard.classList.add('testimonial-card');
+		newCard.innerHTML = `
+		<div class="testimonial-card__body">
+			<div class="testimonial-card__quotation-mark-container">
+				<img class="testimonial-card__quotation-mark" src="images/SVGs/opening-quotation-mark.svg"
+					alt="opening quotation mark" />
+			</div>
+			<p class="testimonial-card__quote paragraph">
+				${quote}
+			</p>
+		</div>
+		<div class="testimonial-card__footer">
+			<div class="photo-small">
+				<img class="photo-small__img" src="${imgUrl}" alt="${fullName}">
+			</div>
+			<div class="testimonial-card__member-details">
+				<p class="testimonial-card__member-name">${fullName} &lsquo;${year}</p>
+				<p class="testimonial-card__member-position">${position}</p>
+			</div>
+		</div>
+		`;
+
+		testimonialCards.push(newCard);
+	});
+
+	// append first 3 elements of array to container of testimonial cards
+	const testimonialsContainer = document.querySelector('#member-testimonials-carousel .testimonial-cards');
+	for (let i = 0; i < testimonialCards.length && i < 3; ++i) {
+		testimonialsContainer.append(testimonialCards[i]);
+	}
+
+	// Create carousel for member testimonial cards cards
 	const memberTestimonialsCarouselOptions = {
 		carouselID: 'member-testimonials-carousel',
 		itemsContainerClass: 'testimonial-cards',
+		itemElemClass: 'testimonial-card',
 		itemElemsArray: testimonialCards,
 		numItems: testimonialCards.length,
 		breakpoint1: 1400,
@@ -401,5 +445,33 @@ const wait = milliseconds =>
 	};
 	const memberTestimonialsCarousel = new Carousel(memberTestimonialsCarouselOptions);
 	memberTestimonialsCarousel.initialize();
+
+
+	
+	/* ************************************** */
+	/* *************NEWS AND EVENTS************* */
+	/* ************************************** */
+	const newsAndEventsCardsContainer = document.querySelector('#news-and-events-carousel .info-cards');
+	const newsAndEventsCards = [...newsAndEventsCardsContainer.querySelectorAll('.info-card')];
+	// empty the container so that we can refill it with just 3 of the items
+	newsAndEventsCardsContainer.innerHTML = '';
+
+	// refill the container using the first 3 cards
+	for (let i = 0; i < newsAndEventsCards.length && i < 3; ++i) {
+		newsAndEventsCardsContainer.append(newsAndEventsCards[i]);
+	}
+
+	// Create carousel for news and events cards
+	const newsAndEventsCarouselOptions = {
+		carouselID: 'news-and-events-carousel',
+		itemsContainerClass: 'info-cards',
+		itemElemClass: 'info-card',
+		itemElemsArray: newsAndEventsCards,
+		numItems: newsAndEventsCards.length,
+		breakpoint1: 1390,
+		breakpoint2: 976,
+	};
+	const newsAndEventsCarousel = new Carousel(newsAndEventsCarouselOptions);
+	newsAndEventsCarousel.initialize();
 })();
 
